0a8099e889ee2e4735f296b08d02a1e3ae0d4211
Wladmir Pinheiro <85358413+wladmirpinheiro@users.noreply.github.com>
2022-02-23 17:46:07 +0000

    Questão 1

---
+++ RevisaoLogica/q2.c
@@ -0,0 +1,14 @@
+//Faça um programa que imprima o seu nome, sua matrícula, e o seu semestre de ingresso no curso. Cada informação em uma linha.
+
+#include <stdio.h>
+int main ()
+{
+
+printf("Nome: Wladmir Pinheiro\n");
+printf("Semestre: 2021202120212021\n");
+printf("Período: 2 semestre\n");
+
+
+
+  return 0;
+}
